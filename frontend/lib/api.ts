6f2dba07c1a7cee3887d5cc09e930ce908159b2b
rloss <rlossqianss@gmail.com>
2025-05-07 23:47:18 +0900

    frontend/lib api.ts

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,1 +1,9 @@
-// API 함수
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export async function fetchPosts() {
+  const res = await fetch(`${API_URL}/api/v1/posts`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch posts");
+  }
+  return res.json();
+}
